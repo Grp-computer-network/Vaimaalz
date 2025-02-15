81b75be5d15716b5938c3406abaa4b5326318064
Grp-computer-network <krithickprofessional@gmail.com>
2025-02-15 08:24:13 +0530

    hello

--- script.js
+++ script.js
@@ -14,3 +14,10 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+document.getElementById("openBtn").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
+});
+
+document.getElementById("closeBtn").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+});
